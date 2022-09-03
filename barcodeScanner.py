ed058e065d2e75dd9e3d61804ede1e63cb925fbc
mayha1 <dinhhoangnhatminh2002@gmail.com>
2022-09-03 16:07:59 +0700

    change names

--- barcodeScanner.py
+++ barcodeScanner.py
@@ -1,18 +1,15 @@
-import pyautogui
-
+from turtle import window_width
 import numpy as np
 import cv2
 from pyzbar import pyzbar
 
-
-screenWidth, screenHeight= pyautogui.size()
-GREEN = (0, 255, 0)
-WHITE = (255, 255, 255)
+WINDOW_WIDTH = 1500
 RED = (0, 0, 255)
 THICKNESS = 2
 FONT = cv2.FONT_HERSHEY_DUPLEX
 FONT_SCALE = 1
 TEXT_THICKNESS = 1
+windowName = 'Barcode scanner'
  
 
 def readBarcode(frame):
@@ -32,26 +29,23 @@ def readBarcode(frame):
         cv2.putText(frame, barcodeData, (x, y), FONT, FONT_SCALE, RED, TEXT_THICKNESS)
         
         with open(f"lastRecognizedBarcode.txt", mode ='w') as file:
-            file.write(f"LAST RECOGNIZED BARCODE INFORMATION \n \
-                Data: {barcodeData} \n \
-                Type: {barcodeType}")
+            file.write(f"LAST RECOGNIZED BARCODE INFORMATION \n Data: {barcodeData} \n Type: {barcodeType}")
     return frame
 
 
 def main():
     camera = cv2.VideoCapture(0)
-    camera.set(cv2.CAP_PROP_FRAME_WIDTH, screenWidth)
-    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, screenHeight)
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, WINDOW_WIDTH)
     ret, frame = camera.read()
 
     while ret:
         ret, frame = camera.read()
         frame = readBarcode(frame)
-        cv2.imshow('Barcode code scanner', frame)
+        cv2.imshow(windowName, frame)
         keyCode = cv2.waitKey(1)
         if keyCode & 0xFF == ord('q'):
             break
-        if cv2.getWindowProperty('Barcode code scanner', cv2.WND_PROP_VISIBLE) <1:
+        if cv2.getWindowProperty(windowName, cv2.WND_PROP_VISIBLE) < 1:
             break
     camera.release()
     cv2.destroyAllWindows()
